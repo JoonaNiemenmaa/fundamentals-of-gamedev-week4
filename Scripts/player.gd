f36e9a8a92ab32fb48c8a8d8a9a801a88b35438d
Joona <joonaoskari@hotmail.com>
2025-09-10 14:03:49 +0300

    task 2 tehty

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,11 +3,22 @@ class_name Player extends CharacterBody2D
 @export var move_speed := 80
 @export var jump_height := 80
 var gravity := float(ProjectSettings.get_setting("physics/2d/default_gravity"))
+@onready var animated_sprite := $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	var move_x := Input.get_axis("LEFT", "RIGHT")
 
 	velocity.x = move_x * move_speed
+
+	if move_x > 0:
+		animated_sprite.flip_h = true
+	elif move_x < 0:
+		animated_sprite.flip_h = false
+
+	if move_x:
+		animated_sprite.play("run")
+	else:
+		animated_sprite.play("idle")
 
 	if not is_on_floor():
 		velocity.y += gravity * delta
